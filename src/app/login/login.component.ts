3019e2e03e041bd38f6c557cac0d04420995ee9f
CristhianMotoche <cristhian.motoche@gmail.com>
2017-07-09 14:49:52 -0500

    Make request to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _http: Http) { }
 
   ngOnInit() {
   }
 
+  login(email, password){
+    this._http
+      .post("http://localhost:1337/login", this.login)
+      .subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {}
+      )
+  }
 }
