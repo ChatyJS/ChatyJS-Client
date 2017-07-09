5b444c8f0a602071639b4ea8dbbec7267e1ef851
CristhianMotoche <cristhian.motoche@gmail.com>
2017-07-09 13:39:21 -0500

    Add login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     NgbModule.forRoot(),
