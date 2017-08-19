b1f73e891da4b19e56ad36e31b33ab2bdfd8bc1a
CristhianMotoche <cristhian.motoche@gmail.com>
2017-08-19 09:05:07 -0500

    Show messages

--- src/app/_components/dashboard/dashboard.component.ts
+++ src/app/_components/dashboard/dashboard.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../_models/User';
+import { Message } from '../../_models/Messages';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  choosedList: string;
+  messages:Message[] = [];
+  users:User[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.choosedList = 'conversations';
   }
 
-  ngOnInput() {
+  choose(listType) {
+    this.choosedList = listType;
+  }
+
+  chooseConversationEvent(event){
+    this.messages = event.channel.messages;
+    this.users = event.channel.users;
   }
 }
--- src/app/_components/messages/messages.component.ts
+++ src/app/_components/messages/messages.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { User } from '../../_models/User';
+import { Message } from '../../_models/Messages';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+  @Input() messages:Message[];
+  @Input() users:User[];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnInput(){
+  }
+
+  getUser(id){
+    return this.users.find( user => user.id == id).name;
+  }
 }
